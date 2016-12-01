199f10905da6b570aa1756eaed61b6bbae56b301
Pablololo12 <pabloheralm@gmail.com>
2016-12-01 23:16:02 +0100

    Script matlab por terminar, y prueba de governor

--- transferencia.m
+++ transferencia.m
@@ -1,14 +1,25 @@
-fileID = fopen('/Users/Pablo/Documents/Universidad/governor-tfg/resultados/4cpu/estado_1728000.txt','r');
-formatSpec = '%d';
-A = fscanf(fileID, formatSpec);
+dirlist = dir('/Users/Pablo/Documents/Universidad/governor-tfg/resultados/4cpu/');
+A = [];
+for i = 1:length(dirlist)
+    nombre = dirlist(i).name;
+    if strcmp(nombre,'.')==0 && strcmp(nombre,'..')==0 && strcmp(nombre,'.DS_Store')==0
+        s = strcat('/Users/Pablo/Documents/Universidad/governor-tfg/resultados/4cpu/',nombre);
+        disp(s);
+        fileID = fopen(s,'r');
+        formatSpec = '%d';
+        B = fscanf(fileID, formatSpec);
+        fclose(fileID);
+        A = [A ; B];
+    end
+end
 Ts = 0.05; %Se toman muestras cada 3 segundos
 F = 0:size(A)-1;
 F=F';
 F(:)=1728000;
 
 data = iddata(A,F,Ts); %pt señal de salida (temp) %ct señal de entrada (freq) %Ts intervalo entre medidas
-data.InputName  = '\Delta CTemp';
-data.InputUnit  = 'C';
+data.InputName  = '\Delta Freq';
+data.InputUnit  = 'KHz';
 data.OutputName = '\Delta PTemp';
 data.OutputUnit = 'C';
 data.TimeUnit   = 'minutes';
@@ -21,7 +32,7 @@ plot(data)
 % described by a first order system with delay. Use the |tfest| command
 % specifying one pole, no zeroes, and an unknown input/output delay to
 % estimate a transfer function.
-sysTF = tfest(data,1,0,nan)
+sysTF = tfest(data,1,0,nan);
 
 %%
 % The |compare| and |resid| commands allow us to investigate how well the
