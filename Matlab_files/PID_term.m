9903797693d5ae63c9bf1f36ce5e8a7dea293563
Pablololo12 <pabloheralm@gmail.com>
2017-05-25 16:45:35 +0200

    Añadidos resultados

--- Matlab_files/PID_term.m
+++ Matlab_files/PID_term.m
@@ -1,9 +1,9 @@
 clear all
 
-Ti = 90;
-Td = 5;
-K = 15000;
-alfa = 0.9; %% Entre 0 y 1
+Ti = 70;
+Td = 0;
+K = 10000;
+alfa = 1; %% Entre 0 y 1
 
 s = tf('s');
 C = K*(Ti*s+1)/Ti/s*(Td*s+1)/(alfa*Td*s+1)
@@ -13,8 +13,8 @@ Cd = c2d(C,Ts)
 [num,den] = tfdata(Cd);
 A = num{1}(1)
 B = num{1}(2)
-C = num{1}(3)
+%C = num{1}(3)
 D = den{1}(1)
 E = den{1}(2)
-F = den{1}(3)
+%F = den{1}(3)
 
