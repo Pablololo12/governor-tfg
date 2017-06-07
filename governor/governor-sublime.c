20341b02738d8c204a482ee0705ca19adda80621
pablololo12 <pabloheralm@gmail.com>
2017-06-07 10:12:32 +0200

    Añadidos metodos en governor

--- governor/governor-sublime.c
+++ governor/governor-sublime.c
@@ -12,27 +12,7 @@ static int A = 50000;
 static int B = -49722;
 static int C = 0;
 
-
-
-/*************sysfs*****************************************/
-
-static struct governor_attr E_value =					\
-__ATTR(E_value, 0644, show_E_value, store_E_value)
-
-static struct governor_attr F_value =					\
-__ATTR(F_value, 0644, show_F_value, store_F_value)
-
-static struct governor_attr A_value =					\
-__ATTR(A_value, 0644, show_A_value, store_A_value)
-
-static struct governor_attr B_value =					\
-__ATTR(B_value, 0644, show_B_value, store_B_value)
-
-static struct governor_attr C_value =					\
-__ATTR(C_value, 0644, show_C_value, store_C_value)
-
-static struct governor_attr temp_obj =					\
-__ATTR(temp_obj, 0644, show_temp_obj, store_temp_obj)
+static DEFINE_MUTEX(access_variables);
 
 
 static int cpufreq_pid_governor_init(struct cpufreq_policy *policy)
@@ -59,6 +39,52 @@ static void cpufreq_pid_governor_limits(struct cpufreq_policy *policy)
 {
 
 }
+
+/*************sysfs*****************************************/
+
+static ssize_t show_E_value(struct gov_attr_set *attr_set, char *buf)
+{
+	int aux;
+	mutex_lock(&access_variables);
+	aux = E;
+	mutex_unlock(&access_variables);
+	return sprintf(buf, "%d\n", aux);
+}
+
+static ssize_t store_E_value(struct gov_attr_set *attr_set,
+				const char *buf, size_t count)
+{
+	unsigned int input;
+	int ret;
+	ret = sscanf(buf, "%d", &input);
+
+	if(ret == NULL) return -EINVAL;
+
+	mutex_lock(&access_variables);
+	E = input;
+	mutex_unlock(&access_variables);
+
+	return count;
+}
+
+static struct governor_attr E_value =					\
+__ATTR(E_value, 0644, show_E_value, store_E_value)
+
+static struct governor_attr F_value =					\
+__ATTR(F_value, 0644, show_F_value, store_F_value)
+
+static struct governor_attr A_value =					\
+__ATTR(A_value, 0644, show_A_value, store_A_value)
+
+static struct governor_attr B_value =					\
+__ATTR(B_value, 0644, show_B_value, store_B_value)
+
+static struct governor_attr C_value =					\
+__ATTR(C_value, 0644, show_C_value, store_C_value)
+
+static struct governor_attr temp_obj =					\
+__ATTR(temp_obj, 0644, show_temp_obj, store_temp_obj)
+
 
 static struct cpufreq_governor cpufreq_gov_pidgovernor = {
 	.name		= "PID_governor",
