09a4434374eb663379a775d840188a4dba3ec39f
Pablololo12 <pabloheralm@gmail.com>
2017-07-10 19:59:44 +0200

    Nuevo governor

--- governor/PID_governor.c
+++ governor/PID_governor.c
@@ -1,14 +1,19 @@
-
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/cpufreq.h>
 #include <linux/cpu.h>
-#include <linux/percpu-defs.h>
-#include <linux/slab.h>
+#include <linux/jiffies.h>
+#include <linux/kernel_stat.h>
+#include <linux/mutex.h>
+#include <linux/hrtimer.h>
 #include <linux/tick.h>
-#include "cpufreq_governor.h"
+#include <linux/ktime.h>
+#include <linux/sched.h>
 #include <linux/thermal.h>
 #include "../thermal/thermal_core.h"
-
 
 /* PID_governor macros */
 #define DEF_E_VALUE		(-1)
@@ -19,53 +24,53 @@
 #define DEF_TEMP_OBJ		(85000)
 #define DEF_SAMPLING_VALUE		(3000000)
 
-static DEFINE_MUTEX(access_variables);
-static DEFINE_MUTEX(access_temp);
-static int temp=0;
-
-static struct workqueue_struct *input_wq;
-static DEFINE_PER_CPU(struct work_struct, dbs_refresh_work);
-
-static DEFINE_PER_CPU(struct cpu_dbs_info_s, cpu_dbs_info);
-
-//static struct od_ops od_ops;
-
 struct cpu_dbs_info_s {
-	unsigned int sampling_rate;
-	int E_value;
-	int F_value;
-	int A_value;
-	int B_value;
-	int C_value;
-	int temp_obj;
+	int error1 = 0;
+	int error2 = 0;
+	int u1 = 0;
+	int u2 = 0;
+	struct cpufreq_policy *cur_policy;
+	struct delayed_work work;
+	unsigned int requested_freq;
+	int cpu;
+	unsigned int enable:1;
+	/*
+	 * percpu mutex that serializes governor limit change with
+	 * do_dbs_timer invocation. We do not want do_dbs_timer to run
+	 * when user is changing the governor or limits.
+	 */
+	struct mutex timer_mutex;
 };
-
-#ifndef CONFIG_CPU_FREQ_DEFAULT_GOV_PIDGOV
-static struct cpufreq_governor cpufreq_gov_pid;
-#endif
+static DEFINE_PER_CPU(struct cpu_dbs_info_s, cs_cpu_dbs_info);
 
 /*
- * Not all CPUs want IO time to be accounted as busy; this depends on how
- * efficient idling at a higher frequency/voltage is.
- * Pavel Machek says this is not so for various generations of AMD and old
- * Intel systems.
- * Mike Chan (android.com) claims this is also not true for ARM.
- * Because of this, whitelist specific known (series) of CPUs by default, and
- * leave all others up to the user.
+ * dbs_mutex protects dbs_enable in governor start/stop.
  */
-static int should_io_be_busy(void)
-{
-#if defined(CONFIG_X86)
-	/*
-	 * For Intel, Core 2 (model 15) and later have an efficient idle.
-	 */
-	if (boot_cpu_data.x86_vendor == X86_VENDOR_INTEL &&
-			boot_cpu_data.x86 == 6 &&
-			boot_cpu_data.x86_model >= 15)
-		return 1;
-#endif
-	return 0;
-}
+static DEFINE_MUTEX(dbs_mutex);
+
+static struct dbs_tuners {
+	unsigned int sampling_rate;
+	unsigned int E_value;
+	unsigned int F_value;
+	unsigned int A_value;
+	unsigned int B_value;
+	unsigned int C_value;
+	unsigned int temp_obj;
+} dbs_tuners_ins = {
+	.sampling_rate = DEF_SAMPLING_VALUE,
+	.E_value = DEF_E_VALUE,
+	.F_value = DEF_F_VALUE,
+	.A_value = DEF_A_VALUE,
+	.B_value = DEF_B_VALUE,
+	.C_value = DEF_C_VALUE,
+	.temp_obj = DEF_TEMP_OBJ,
+};
+
+
+/********** Pray to work ************/
+
+static DEFINE_MUTEX(access_temp);
+static int temp=0; // TODO: Ponerla en otro lado
 
 static int update_temp(struct thermal_zone_device *tz, int trip)
 {
@@ -75,226 +80,191 @@ static int update_temp(struct thermal_zone_device *tz, int trip)
 	return 0;
 }
 
-/*
- * Every sampling_rate, we check, if current idle time is less than 20%
- * (default), then we try to increase frequency. Else, we adjust the frequency
- * proportional to load.
- */
-static void pid_update(int cpu)
+/**************** A little bit of magic ****************/
+
+/******************* Working queues *******************/
+/* TODO: Explain this */
+
+static void do_dbs_timer(struct work_struct *work)
 {
-	// Global variables
-	static int u1 = 0;
-	static int u2 = 0;
-	static int e1 = 0;
-	static int e2 = 0;
-	int E, F, A, B, C, error, u;
+	struct cpu_dbs_info_s *dbs_info =
+		container_of(work, struct cpu_dbs_info_s, work.work);
+	unsigned int cpu = dbs_info->cpu;
 
-	struct od_cpu_dbs_info_s *dbs_info = &per_cpu(od_cpu_dbs_info, cpu);
-	struct cpufreq_policy *policy = dbs_info->cdbs.shared->policy;
-	struct dbs_data *dbs_data = policy->governor_data;
-	struct pid_dbs_tuners *pid_tuners = dbs_data->tuners;
+	/* We want all CPUs to do sampling nearly on same jiffy */
+	mutex_lock(&dbs_mutex);
+	int delay = usecs_to_jiffies(dbs_tuners_ins.sampling_rate);
+	mutex_unlock(&dbs_mutex);
 
-	int temp_ac=0;
-	mutex_lock(&access_temp);
-	temp_ac=temp;
-	mutex_unlock(&access_temp);
+	delay -= jiffies % delay;
 
-	mutex_lock(&access_variables);
-	E = pid_tuners->E_value;
-	F = pid_tuners->F_value;
-	A = pid_tuners->A_value;
-	B = pid_tuners->B_value;
-	C = pid_tuners->C_value;
-	error = pid_tuners->temp_obj - temp_ac; // OJO!! Obtener temperatura
-	mutex_unlock(&access_variables);
+	mutex_lock(&dbs_info->timer_mutex);
 
-	u = -E * u1 - F * u2 + A * error + B * e1 + C * e2;
+	dbs_check_cpu(dbs_info);
 
-	u2 = u1;
-	u1 = u;
-	e2 = e1;
-	e1 = error;
-	
-	__cpufreq_driver_target(policy, u, CPUFREQ_RELATION_C);
+	schedule_delayed_work_on(cpu, &dbs_info->work, delay);
+	mutex_unlock(&dbs_info->timer_mutex);
 }
 
-static unsigned int pid_dbs_timer(struct cpu_dbs_info *cdbs,
-				 struct dbs_data *dbs_data, bool modify_all)
+static inline void dbs_timer_init(struct cpu_dbs_info_s *dbs_info)
 {
-	struct cpufreq_policy *policy = cdbs->shared->policy;
-	unsigned int cpu = policy->cpu;
-	struct od_cpu_dbs_info_s *dbs_info = &per_cpu(od_cpu_dbs_info,
-			cpu);
-	struct pid_dbs_tuners *od_tuners = dbs_data->tuners;
-	int delay = 0, sample_type = dbs_info->sample_type;
+	/* We want all CPUs to do sampling nearly on same jiffy */
+	int delay = usecs_to_jiffies(dbs_tuners_ins.sampling_rate);
+	delay -= jiffies % delay;
 
-	if (!modify_all)
-		goto max_delay;
-
-	pid_update(cpu);
-
-max_delay:
-	if (!delay)
-		delay = delay_for_sampling_rate(od_tuners->sampling_rate
-				* dbs_info->rate_mult);
-
-	return delay;
+	dbs_info->enable = 1;
+	INIT_DELAYED_WORK_DEFERRABLE(&dbs_info->work, do_dbs_timer);
+	schedule_delayed_work_on(dbs_info->cpu, &dbs_info->work, delay);
 }
+
+static inline void dbs_timer_exit(struct cpu_dbs_info_s *dbs_info)
+{
+	dbs_info->enable = 0;
+	cancel_delayed_work_sync(&dbs_info->work);
+}
+
+/**************** End of magic ****************/
+
+
 
 /************************** sysfs interface ************************/
-static struct common_dbs_data pid_dbs_cdata;
+/* cpufreq_conservative Governor Tunables */
+#define show_one(file_name, object)					\
+static ssize_t show_##file_name						\
+(struct kobject *kobj, struct attribute *attr, char *buf)		\
+{									\
+	return sprintf(buf, "%u\n", dbs_tuners_ins.object);		\
+}
 
-static ssize_t store_sampling_rate(struct dbs_data *dbs_data, const char *buf,
-		size_t count)
+show_one(sampling_rate, sampling_rate);
+show_one(E_value, E_value);
+show_one(F_value, F_value);
+show_one(A_value, A_value);
+show_one(B_value, B_value);
+show_one(C_value, C_value);
+show_one(temp_obj, temp_obj);
+
+static ssize_t store_sampling_rate(struct kobject *a, struct attribute *b,
+				   const char *buf, size_t count)
 {
-	struct pid_dbs_tuners *pid_tuners = dbs_data->tuners;
 	unsigned int input;
 	int ret;
 	ret = sscanf(buf, "%u", &input);
 	if (ret != 1)
 		return -EINVAL;
 
-	pid_tuners->sampling_rate = input;
+	mutex_lock(&dbs_mutex);
+	dbs_tuners_ins.sampling_rate = input;
+	mutex_unlock(&dbs_mutex);
 
 	return count;
 }
 
-static ssize_t store_E_value(struct dbs_data *dbs_data, const char *buf,
-		size_t count)
+static ssize_t store_E_value(struct kobject *a, struct attribute *b,
+				   const char *buf, size_t count)
 {
-	struct pid_dbs_tuners *pid_tuners = dbs_data->tuners;
 	int input;
 	int ret;
 	ret = sscanf(buf, "%d", &input);
 
 	if(ret != 1) return -EINVAL;
 
-	mutex_lock(&access_variables);
-	pid_tuners->E_value = input;
-	mutex_unlock(&access_variables);
+	mutex_lock(&dbs_mutex);
+	dbs_tuners_ins.E_value = input;
+	mutex_unlock(&dbs_mutex);
 
 	return count;
 }
 
-static ssize_t store_F_value(struct dbs_data *dbs_data, const char *buf,
-		size_t count)
+static ssize_t store_F_value(struct kobject *a, struct attribute *b,
+				   const char *buf, size_t count)
 {
-	struct pid_dbs_tuners *pid_tuners = dbs_data->tuners;
 	int input;
 	int ret;
 	ret = sscanf(buf, "%d", &input);
 
 	if(ret != 1) return -EINVAL;
 
-	mutex_lock(&access_variables);
-	pid_tuners->F_value = input;
-	mutex_unlock(&access_variables);
+	mutex_lock(&dbs_mutex);
+	dbs_tuners_ins.F_value = input;
+	mutex_unlock(&dbs_mutex);
 
 	return count;
 }
-static ssize_t store_A_value(struct dbs_data *dbs_data, const char *buf,
-		size_t count)
+static ssize_t store_A_value(struct kobject *a, struct attribute *b,
+				   const char *buf, size_t count)
 {
-	struct pid_dbs_tuners *pid_tuners = dbs_data->tuners;
 	int input;
 	int ret;
 	ret = sscanf(buf, "%d", &input);
 
 	if(ret != 1) return -EINVAL;
 
-	mutex_lock(&access_variables);
-	pid_tuners->A_value = input;
-	mutex_unlock(&access_variables);
+	mutex_lock(&dbs_mutex);
+	dbs_tuners_ins.A_value = input;
+	mutex_unlock(&dbs_mutex);
 
 	return count;
 }
 
-static ssize_t store_B_value(struct dbs_data *dbs_data, const char *buf,
-		size_t count)
+static ssize_t store_B_value(struct kobject *a, struct attribute *b,
+				   const char *buf, size_t count)
 {
-	struct pid_dbs_tuners *pid_tuners = dbs_data->tuners;
 	int input;
 	int ret;
 	ret = sscanf(buf, "%d", &input);
 
 	if(ret != 1) return -EINVAL;
 
-	mutex_lock(&access_variables);
-	pid_tuners->B_value = input;
-	mutex_unlock(&access_variables);
+	mutex_lock(&dbs_mutex);
+	dbs_tuners_ins.B_value = input;
+	mutex_unlock(&dbs_mutex);
 
 	return count;
 }
 
-static ssize_t store_C_value(struct dbs_data *dbs_data, const char *buf,
-		size_t count)
+static ssize_t store_C_value(struct kobject *a, struct attribute *b,
+				   const char *buf, size_t count)
 {
-	struct pid_dbs_tuners *pid_tuners = dbs_data->tuners;
 	int input;
 	int ret;
 	ret = sscanf(buf, "%d", &input);
 
 	if(ret != 1) return -EINVAL;
 
-	mutex_lock(&access_variables);
-	pid_tuners->C_value = input;
-	mutex_unlock(&access_variables);
+	mutex_lock(&dbs_mutex);
+	dbs_tuners_ins.C_value = input;
+	mutex_unlock(&dbs_mutex);
 
 	return count;
 }
 
-static ssize_t store_temp_obj(struct dbs_data *dbs_data, const char *buf,
-		size_t count)
+static ssize_t store_temp_obj(struct kobject *a, struct attribute *b,
+				   const char *buf, size_t count)
 {
-	struct pid_dbs_tuners *pid_tuners = dbs_data->tuners;
 	int input;
 	int ret;
 	ret = sscanf(buf, "%d", &input);
 
 	if(ret != 1) return -EINVAL;
 
-	mutex_lock(&access_variables);
-	pid_tuners->temp_obj = input;
-	mutex_unlock(&access_variables);
+	mutex_lock(&dbs_mutex);
+	dbs_tuners_ins.temp_obj = input;
+	mutex_unlock(&dbs_mutex);
 
 	return count;
 }
 
 
-show_store_one(pid, sampling_rate);
-show_store_one(pid, E_value);
-show_store_one(pid, F_value);
-show_store_one(pid, A_value);
-show_store_one(pid, B_value);
-show_store_one(pid, C_value);
-show_store_one(pid, temp_obj);
+define_one_global_rw(sampling_rate);
+define_one_global_rw(E_value);
+define_one_global_rw(F_value);
+define_one_global_rw(A_value);
+define_one_global_rw(B_value);
+define_one_global_rw(C_value);
+define_one_global_rw(temp_obj);
 
-gov_sys_pol_attr_rw(sampling_rate);
-gov_sys_pol_attr_rw(E_value);
-gov_sys_pol_attr_rw(F_value);
-gov_sys_pol_attr_rw(A_value);
-gov_sys_pol_attr_rw(B_value);
-gov_sys_pol_attr_rw(C_value);
-gov_sys_pol_attr_rw(temp_obj);
-
-
-static struct attribute *dbs_attributes_gov_sys[] = {
-	&sampling_rate_gov_sys.attr,
-	&E_value_gov_sys.attr,
-	&F_value_gov_sys.attr,
-	&A_value_gov_sys.attr,
-	&B_value_gov_sys.attr,
-	&C_value_gov_sys.attr,
-	&temp_obj_gov_sys.attr,
-	NULL
-};
-
-static struct attribute_group pid_attr_group_gov_sys = {
-	.attrs = dbs_attributes_gov_sys,
-	.name = "PID_governor",
-};
-
-static struct attribute *dbs_attributes_gov_pol[] = {
+static struct attribute *dbs_attributes[] = {
 	&sampling_rate_gov_pol.attr,
 	&E_value_gov_pol.attr,
 	&F_value_gov_pol.attr,
@@ -305,70 +275,140 @@ static struct attribute *dbs_attributes_gov_pol[] = {
 	NULL
 };
 
-static struct attribute_group pid_attr_group_gov_pol = {
-	.attrs = dbs_attributes_gov_pol,
+static struct attribute_group pid_attr_group = {
+	.attrs = dbs_attributes,
 	.name = "PID_governor",
 };
 
-/************************** sysfs end ************************/
+/********************* end sysfs ******************************/
 
-static int pid_init(struct dbs_data *dbs_data, bool notify)
+static struct notifier_block dbs_cpufreq_notifier_block = {
+	.notifier_call = dbs_cpufreq_notifier
+};
+
+/********************* PID como tal *********************/
+static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 {
-	struct pid_dbs_tuners *tuners;
-	u64 idle_time;
-	int cpu;
+	int E, F, A, B, C, error, u, e1, e2, u1, u2;
+	struct cpufreq_policy *policy;
 
-	tuners = kzalloc(sizeof(*tuners), GFP_KERNEL);
-	if (!tuners) {
-		pr_err("%s: kzalloc failed\n", __func__);
-		return -ENOMEM;
+	policy = this_dbs_info->cur_policy;
+
+	int temp_ac=0;
+	mutex_lock(&access_temp);
+	temp_ac=temp;
+	mutex_unlock(&access_temp);
+
+	e1 = this_dbs_info->error1;
+	e2 = this_dbs_info->error2;
+	u1 = this_dbs_info->u1;
+	u2 = this_dbs_info->u2;
+
+	mutex_lock(&dbs_mutex);
+	E = dbs_tuners_ins.E_value;
+	F = dbs_tuners_ins.F_value;
+	A = dbs_tuners_ins.A_value;
+	B = dbs_tuners_ins.B_value;
+	C = dbs_tuners_ins.C_value;
+	error = dbs_tuners_ins.temp_obj - temp_ac; // OJO!! Obtener temperatura
+	mutex_unlock(&dbs_mutex);
+
+	u = -E * u1 - F * u2 + A * error + B * e1 + C * e2;
+
+	u2 = u1;
+	u1 = u;
+	e2 = e1;
+	e1 = error;
+
+	this_dbs_info->error1 = e1;
+	this_dbs_info->error2 = e2;
+	this_dbs_info->u1 = u1;
+	this_dbs_info->u2 = u2;
+	
+	__cpufreq_driver_target(policy, u, CPUFREQ_RELATION_C);
+}
+
+/**************** Inicializacion del governor *****************/
+
+static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
+				   unsigned int event)
+{
+	unsigned int cpu = policy->cpu;
+	struct cpu_dbs_info_s *this_dbs_info;
+	unsigned int j;
+	int rc;
+
+	this_dbs_info = &per_cpu(cs_cpu_dbs_info, cpu);
+
+	switch (event) {
+	case CPUFREQ_GOV_START:
+		if ((!cpu_online(cpu)) || (!policy->cur))
+			return -EINVAL;
+
+		mutex_lock(&dbs_mutex);
+
+		for_each_cpu(j, policy->cpus) {
+			struct cpu_dbs_info_s *j_dbs_info;
+			j_dbs_info = &per_cpu(cs_cpu_dbs_info, j);
+			j_dbs_info->cur_policy = policy;
+		}
+
+		mutex_init(&this_dbs_info->timer_mutex);
+		dbs_enable++;
+		/*
+		 * Start the timerschedule work, when this governor
+		 * is used for first time
+		 */
+		if (dbs_enable == 1) {
+			unsigned int latency;
+			/* policy latency is in nS. Convert it to uS first */
+			latency = policy->cpuinfo.transition_latency / 1000;
+			if (latency == 0)
+				latency = 1;
+
+			rc = sysfs_create_group(cpufreq_global_kobject,
+						&dbs_attr_group);
+			if (rc) {
+				mutex_unlock(&dbs_mutex);
+				return rc;
+			}
+
+			cpufreq_register_notifier(
+					&dbs_cpufreq_notifier_block,
+					CPUFREQ_TRANSITION_NOTIFIER);
+		}
+		mutex_unlock(&dbs_mutex);
+
+		dbs_timer_init(this_dbs_info);
+
+		break;
+
+	case CPUFREQ_GOV_STOP:
+		dbs_timer_exit(this_dbs_info);
+
+		mutex_lock(&dbs_mutex);
+		dbs_enable--;
+		mutex_destroy(&this_dbs_info->timer_mutex);
+
+		/*
+		 * Stop the timerschedule work, when this governor
+		 * is used for first time
+		 */
+		if (dbs_enable == 0)
+			cpufreq_unregister_notifier(
+					&dbs_cpufreq_notifier_block,
+					CPUFREQ_TRANSITION_NOTIFIER);
+
+		mutex_unlock(&dbs_mutex);
+		if (!dbs_enable)
+			sysfs_remove_group(cpufreq_global_kobject,
+					   &dbs_attr_group);
+
+		break;
 	}
-
-	tuners->E_value = DEF_E_VALUE;
-	tuners->F_value = DEF_F_VALUE;
-	tuners->A_value = DEF_A_VALUE;
-	tuners->B_value = DEF_B_VALUE;
-	tuners->C_value = DEF_C_VALUE;
-	tuners->temp_obj = DEF_TEMP_OBJ;
-	tuners->sampling_rate = DEF_SAMPLING_VALUE;
-
-	dbs_data->tuners = tuners;
 	return 0;
 }
 
-static void pid_exit(struct dbs_data *dbs_data, bool notify)
-{
-	kfree(dbs_data->tuners);
-}
-
-define_get_cpu_dbs_routines(od_cpu_dbs_info);
-
-static struct od_ops od_ops = {
-	.powersave_bias_init_cpu = NULL,
-	.powersave_bias_target = NULL,
-	.freq_increase = NULL,
-};
-
-#define PID_GOVERNOR		2
-static struct common_dbs_data pid_dbs_cdata = {
-	.governor = PID_GOVERNOR,
-	.attr_group_gov_sys = &pid_attr_group_gov_sys,
-	.attr_group_gov_pol = &pid_attr_group_gov_pol,
-	.get_cpu_cdbs = get_cpu_cdbs,
-	.get_cpu_dbs_info_s = get_cpu_dbs_info_s,
-	.gov_dbs_timer = pid_dbs_timer,
-	.gov_check_cpu = NULL,
-	.gov_ops = &od_ops,
-	.init = pid_init,
-	.exit = pid_exit,
-	.mutex = __MUTEX_INITIALIZER(pid_dbs_cdata.mutex),
-};
-
-static int pid_cpufreq_governor_dbs(struct cpufreq_policy *policy,
-		unsigned int event)
-{
-	return cpufreq_governor_dbs(policy, &pid_dbs_cdata, event);
-}
 
 #ifndef CONFIG_CPU_FREQ_DEFAULT_GOV_PIDGOV
 static
@@ -386,17 +426,7 @@ static struct thermal_governor thermal_gov_user_space = {
 
 static int __init cpufreq_gov_dbs_init(void)
 {
-	unsigned int i;
-
 	if(thermal_register_governor(&thermal_gov_user_space)) return 0;
-
-	for_each_possible_cpu(i) {
-		struct cpu_dbs_info_s *this_dbs_info =
-			&per_cpu(cpu_dbs_info, i);
-		
-		INIT_WORK(&per_cpu(dbs_refresh_work, i), dbs_refresh_callback);
-	}
-
 	return cpufreq_register_governor(&cpufreq_gov_pid);
 }
 
