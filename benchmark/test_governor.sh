2b6552e4d22a703f23cb4e05d816a873bae0e714
pablololo12 <pabloheralm@gmail.com>
2017-08-11 11:51:27 +0200

    Modificado script

--- benchmark/test_governor.sh
+++ benchmark/test_governor.sh
@@ -14,6 +14,11 @@ echo -e "\033[0;36m=============================================================
 
 exec 2>/dev/null
 export LD_LIBRARY_PATH=/home/linaro/gcc-4.8/lib/
+notchange=0
+
+if [ "$#" -eq 1 ]; then
+	notchange=1
+fi
 
 if [ "$#" -ne 6 ]; then
 	echo "Usage: A B C E F TempObj"
@@ -29,29 +34,30 @@ if [ "$?" -ne 0 ]; then
 fi
 echo -e "[\033[0;92mOK\033[0m]"
 
+if [ $notchange = 0 ]; then
+	echo -n "Setting Governor to PID_GOVERNOR..."
+	echo PID_GOVERNOR >/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+	echo PID_GOVERNOR >/sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+	if [ "$?" -ne 0 ]; then
+		echo -e "[\033[0;31mERROR\033[0m]"
+		exit
+	fi
+	echo -e "[\033[0;92mOK\033[0m]"
 
-echo -n "Setting Governor to PID_GOVERNOR..."
-echo PID_GOVERNOR >/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-echo PID_GOVERNOR >/sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
-if [ "$?" -ne 0 ]; then
-	echo -e "[\033[0;31mERROR\033[0m]"
-	exit
+
+	echo -n "Changing parameters of PID"
+	echo "$1" >/sys/devices/system/cpu/cpufreq/PID_governor/A_value
+	echo "$2" >/sys/devices/system/cpu/cpufreq/PID_governor/B_value
+	echo "$3" >/sys/devices/system/cpu/cpufreq/PID_governor/C_value
+	echo "$4" >/sys/devices/system/cpu/cpufreq/PID_governor/E_value
+	echo "$5" >/sys/devices/system/cpu/cpufreq/PID_governor/F_value
+	echo "$6" >/sys/devices/system/cpu/cpufreq/PID_governor/temp_obj
+	if [ "$?" -ne 0 ]; then
+		echo -e "[\033[0;31mERROR\033[0m]"
+		exit
+	fi
+	echo -e "[\033[0;92mOK\033[0m]"
 fi
-echo -e "[\033[0;92mOK\033[0m]"
-
-
-echo -n "Changing parameters of PID"
-echo "$1" >/sys/devices/system/cpu/cpufreq/PID_governor/A_value
-echo "$2" >/sys/devices/system/cpu/cpufreq/PID_governor/B_value
-echo "$3" >/sys/devices/system/cpu/cpufreq/PID_governor/C_value
-echo "$4" >/sys/devices/system/cpu/cpufreq/PID_governor/E_value
-echo "$5" >/sys/devices/system/cpu/cpufreq/PID_governor/F_value
-echo "$6" >/sys/devices/system/cpu/cpufreq/PID_governor/temp_obj
-if [ "$?" -ne 0 ]; then
-	echo -e "[\033[0;31mERROR\033[0m]"
-	exit
-fi
-echo -e "[\033[0;92mOK\033[0m]"
 
 echo "Recording temps"
 ./tempLog.sh &
